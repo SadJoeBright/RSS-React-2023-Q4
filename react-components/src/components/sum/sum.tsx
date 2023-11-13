bee994b68cd455eb33adfa4be74c0b069c2e62e5
SadJoeBright <sadjoebright.gmail.com>
2023-11-13 17:15:06 +0200

    chore: add vitest to project

---
+++ react-components/src/components/sum/sum.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+export default function sum(a: number, b: number) {
+  return a + b;
+}
+
+function squere(a: number, b: number) {
+  return a * b;
+}
